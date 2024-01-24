0f9af024144852644b206e4d661c29085d20f05c
Christopher Bellanger <christopher.bellanger.p@gmail.com>
2024-01-24 11:03:33 +0100

    improve save-trick-button

--- app/modules/trick/components/save-trick-button.tsx
+++ app/modules/trick/components/save-trick-button.tsx
@@ -1,13 +1,30 @@
-import { useRef, useState } from "react";
+import { useState } from "react";
 
 import { useFetcher } from "@remix-run/react";
-import { BookmarkCheckIcon, BookmarkIcon, TrashIcon } from "lucide-react";
+import { BookmarkCheckIcon, BookmarkIcon } from "lucide-react";
 
 import { Button } from "~/modules/ui";
+import { Divider } from "~/modules/ui/divider";
 import { Label } from "~/modules/ui/label";
 import { Popover, PopoverContent, PopoverTrigger } from "~/modules/ui/popover";
 import { RadioGroup, RadioGroupItem } from "~/modules/ui/radio-group";
 import type { action } from "~/routes/save-trick";
+import type { WithChildrenProps } from "~/types";
+
+function getOptions(saved: boolean) {
+	return [
+		{ label: "Want to learn", value: "want-to-learn" },
+		{ label: "Learning", value: "learning" },
+		{ label: "Learned", value: "learned" },
+		...(saved
+			? [{ label: "Remove from collections", value: "", id: "remove" }]
+			: []),
+	];
+}
+
+function RadioGroupBox({ children }: WithChildrenProps) {
+	return <div className="my-1 flex items-center space-x-2">{children}</div>;
+}
 
 interface Props {
 	trickId: string;
@@ -17,48 +34,50 @@ interface Props {
 export function SaveTrickButton({ trickId, category }: Props) {
 	const savedTrick = useFetcher<typeof action>();
 	const [isOpen, setIsOpen] = useState(false);
-	const ref = useRef<HTMLInputElement>(null);
 	const Icon = category ? BookmarkCheckIcon : BookmarkIcon;
+	const buttonContent =
+		savedTrick.state === "loading" ? (
+			"loading"
+		) : (
+			<>
+				<Icon className="mr-2 size-5" />
+				{category || "Save"}
+			</>
+		);
 
 	return (
 		<Popover open={isOpen} onOpenChange={setIsOpen}>
 			<PopoverTrigger asChild>
 				<Button type="submit" className="min-w-20 ">
-					<Icon className="mr-2 size-5" />
-					{category || "Save"}
+					{buttonContent}
 				</Button>
 			</PopoverTrigger>
 			<PopoverContent>
 				<div className="font-medium">Collections</div>
-				<savedTrick.Form method="post" action="/save-trick">
+				<Divider />
+				<savedTrick.Form
+					method="post"
+					action="/save-trick"
+					onChange={(event) => {
+						savedTrick.submit(event.currentTarget, {
+							method: "POST",
+						});
+						setIsOpen(false);
+					}}
+				>
 					<input type="hidden" name="trickId" value={trickId} />
-					<input type="submit" className="hidden" ref={ref} />
-					<RadioGroup
-						defaultValue={category}
-						name="category"
-						onValueChange={() => {
-							setIsOpen(false);
-							ref.current?.click();
-						}}
-					>
-						<div className="flex items-center space-x-2">
-							<RadioGroupItem value="wanted" id="wanted" />
-							<Label htmlFor="wanted">Want to learn</Label>
-						</div>
-						<div className="flex items-center space-x-2">
-							<RadioGroupItem value="learning" id="learning" />
-							<Label htmlFor="learning">Learning</Label>
-						</div>
-						<div className="flex items-center space-x-2">
-							<RadioGroupItem value="learned" id="learned" />
-							<Label htmlFor="learned">Learned</Label>
-						</div>
-						{category && (
-							<div className="flex items-center space-x-2">
-								<TrashIcon className="size-4" />
-								<Label>Remove from collections</Label>
-							</div>
-						)}
+					<RadioGroup defaultValue={category} name="category">
+						{getOptions(Boolean(category)).map((option) => (
+							<RadioGroupBox key={option.value || option.id}>
+								<RadioGroupItem
+									value={option.value}
+									id={option.value || option.id}
+								/>
+								<Label htmlFor={option.value || option.id}>
+									{option.label}
+								</Label>
+							</RadioGroupBox>
+						))}
 					</RadioGroup>
 				</savedTrick.Form>
 			</PopoverContent>
